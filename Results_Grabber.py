a7e2fc2de2827c29a7756bdf73a437fa45d02c5d
Adele Bosma <118491727+APBosma@users.noreply.github.com>
2026-06-30 14:54:34 -0400

    Now Grabs all Results from District
    Since the column lengths need to match, I had it put None if the column was not relevant to the competition.

--- Results_Grabber.py
+++ Results_Grabber.py
@@ -5,11 +5,7 @@ from html_table_parser.parser import HTMLTableParser
 
 # Opens a website and read its
 # binary contents (HTTP Response Body)
-def url_get_contents(url):
-
-    # Opens a website and read its
-    # binary contents (HTTP Response Body)
-
+def urlGetContents(url):
     #making request to the website
     req = urllib.request.Request(url=url)
     f = urllib.request.urlopen(req)
@@ -17,24 +13,97 @@ def url_get_contents(url):
     #reading contents of the website
     return f.read()
 
+def findColumn(table, columnName):
+    for i in range(len(table[0])):
+        if table[0][i] == columnName:
+            return i
+    return -1
+
 # Constants
 DISCTRICTS_COUNT = 32
 REGIONS_COUNT = 4
+SEASON_ID = 18
+CONFERENCE = 3
 
 # Note that 2 is for some reason not a valid grouping id, so we skip it :(
 GROUPING_IDS = [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
 
-# Getting stuff from url
-xhtml = url_get_contents('https://postings.speechwire.com/r-uil-academics.php?groupingid=8&Submit=View+postings&region=&district=11&state=&conference=3&seasonid=18').decode('utf-8')
+data_with_columns = {
+        'Contest_ID': [],
+        'Place': [],
+        'School': [],
+        'Entry': [],
+        'Code': [],
+        'Total': [],
+        'Objective': [],     # Social Studies and Lit Crit
+        'Essay': [],         # Social Studies and Lit Crit
+        'Biology': [],       # Science Only
+        'Chemistry': [],     # Science Only
+        'Physics': []        # Science Only
+    }
 
-# HTMLTableParser object
-p = HTMLTableParser()
-p.feed(xhtml)
+for grouping_id in GROUPING_IDS:
+    # Getting stuff from url
+    xhtml = urlGetContents(f'https://postings.speechwire.com/r-uil-academics.php?groupingid={grouping_id}&Submit=View+postings&region=&district=11&state=&conference={CONFERENCE}&seasonid={SEASON_ID}').decode('utf-8')
 
-# Cleaning up the data
-p.tables[4][0].pop(0)
-p.tables[4][0][0] = 'Place'
+    # HTMLTableParser object
+    p = HTMLTableParser()
+    p.feed(xhtml)
+
+    # Cleaning up the data
+    p.tables[4][0].pop(0)
+    p.tables[4][0][0] = 'Place'
+
+    for row in p.tables[4][1:len(p.tables[4])]:
+        data_with_columns['Contest_ID'].append(grouping_id)
+        data_with_columns['Place'].append(row[0])
+        data_with_columns['School'].append(row[1])
+        data_with_columns['Entry'].append(row[2])
+        data_with_columns['Code'].append(row[3])
+
+    # Handles accounting, calculator, computer science, mathematics, number sense, etc. (Items graded as is)
+    if grouping_id in [1, 7, 8, 9, 10, 11]:
+        for row in p.tables[4][1:len(p.tables[4])]:
+            data_with_columns['Total'].append(row[4])
+            data_with_columns['Biology'].append(None)
+            data_with_columns['Chemistry'].append(None)
+            data_with_columns['Physics'].append(None)
+            data_with_columns['Objective'].append(None)
+            data_with_columns['Essay'].append(None)
+
+    # Handles scienc
+    elif grouping_id == 12:
+        for row in p.tables[4][1:len(p.tables[4])]:
+            data_with_columns['Biology'].append(row[4])
+        for row in p.tables[4][1:len(p.tables[4])]:
+            data_with_columns['Chemistry'].append(row[5])
+        for row in p.tables[4][1:len(p.tables[4])]:
+            data_with_columns['Physics'].append(row[6])
+            data_with_columns['Total'].append(None)
+            data_with_columns['Objective'].append(None)
+            data_with_columns['Essay'].append(None)
+    
+    # Handles social studies and lit crit
+    elif grouping_id in [6, 4]:
+        for row in p.tables[4][1:len(p.tables[4])]:
+            data_with_columns['Objective'].append(row[4])
+        for row in p.tables[4][1:len(p.tables[4])]:
+            data_with_columns['Essay'].append(row[5])
+            data_with_columns['Total'].append(None)
+            data_with_columns['Biology'].append(None)
+            data_with_columns['Chemistry'].append(None)
+            data_with_columns['Physics'].append(None)
+
+    # Everything with no scores listed, just points
+    else:
+        for row in p.tables[4][1:len(p.tables[4])]:
+            data_with_columns['Total'].append(None)
+            data_with_columns['Biology'].append(None)
+            data_with_columns['Chemistry'].append(None)
+            data_with_columns['Physics'].append(None)
+            data_with_columns['Objective'].append(None)
+            data_with_columns['Essay'].append(None)
 
 # converting the parsed data to dataframe
-print("\n\nPANDAS DATAFRAME\n")
-print(pd.DataFrame(p.tables[4]))
+print("\nTable\n")
+print(pd.DataFrame(data_with_columns))
