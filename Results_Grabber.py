5e60e43c4574e4171f02aaef560a44943cd4d9a0
Adele Bosma <118491727+APBosma@users.noreply.github.com>
2026-06-30 13:43:31 -0400

    Adding comments and constants

--- Results_Grabber.py
+++ Results_Grabber.py
@@ -17,17 +17,24 @@ def url_get_contents(url):
     #reading contents of the website
     return f.read()
 
-# defining the html contents of a URL.
+# Constants
+DISCTRICTS_COUNT = 32
+REGIONS_COUNT = 4
+
+# Note that 2 is for some reason not a valid grouping id, so we skip it :(
+GROUPING_IDS = [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+
+# Getting stuff from url
 xhtml = url_get_contents('https://postings.speechwire.com/r-uil-academics.php?groupingid=8&Submit=View+postings&region=&district=11&state=&conference=3&seasonid=18').decode('utf-8')
 
-# Defining the HTMLTableParser object
+# HTMLTableParser object
 p = HTMLTableParser()
 p.feed(xhtml)
 
+# Cleaning up the data
 p.tables[4][0].pop(0)
 p.tables[4][0][0] = 'Place'
 
-# converting the parsed data to
-# dataframe
+# converting the parsed data to dataframe
 print("\n\nPANDAS DATAFRAME\n")
 print(pd.DataFrame(p.tables[4]))
